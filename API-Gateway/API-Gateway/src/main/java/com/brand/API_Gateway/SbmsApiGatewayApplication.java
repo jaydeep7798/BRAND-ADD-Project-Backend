bc4b5ab68f306b96bdf7654bfb0c544ab407d012
Jaydeep <105807221+jaydeep7798@users.noreply.github.com>
2025-10-29 11:59:14 +0530

    testing commit for pull on intellij

--- API-Gateway/API-Gateway/src/main/java/com/brand/API_Gateway/SbmsApiGatewayApplication.java
+++ API-Gateway/API-Gateway/src/main/java/com/brand/API_Gateway/SbmsApiGatewayApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients(basePackages = "com.brand")
 public class SbmsApiGatewayApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SbmsApiGatewayApplication.class, args);
 	}
